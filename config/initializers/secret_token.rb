c44f3f5d6b64f082ced75a7ba7c7883f8c664fef
Troy Murray <troy.murray@gmail.com>
2012-11-05 15:41:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvancedSearch::Application.config.secret_token = '966e8a329beb2f93afd60220a71edfa1fd69af1684665cb072dd22f38cb92c30b5e79ae0dcb260f564c8a7a08e08747a56e1c42611499e1b8ce3fcbf511f6588'
---
+++ db/migrate/20121105204043_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :category_id
+      t.boolean :deleted
+
+      t.timestamps
+    end
+  end
+end
